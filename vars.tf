f38fcc99a995e0ab7a25932b2f5b381c96d943c8
manu <manu@local.com>
2022-05-25 10:55:38 +0530

    Auto Push Commit

--- vars.tf
+++ vars.tf
@@ -1,12 +1,1 @@
-variable "VPC_CIDR" {}
 variable "ENV" {}
-variable "AZ" {}
-variable "PUBLIC_SUBNET_CIDR" {}
-variable "PRIVATE_SUBNET_CIDR" {}
-variable "DEFAULT_VPC_ID" {}
-variable "DEFAULT_VPC_CIDR" {}
-variable "DEFAULT_VPC_RT" {}
-variable "HOSTEDZONE_PRIVATE_ID" {}
-variable "HOSTEDZONE_PRIVATE_NAME" {}
-variable "HOSTEDZONE_PUBLIC_ID" {}
-variable "HOSTEDZONE_PUBLIC_NAME" {}
